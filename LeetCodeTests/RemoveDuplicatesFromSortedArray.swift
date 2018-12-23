e1e3fab22866ab2d8ba72f221884e6913451a45e
RandallWang <randallwang93@gmail.com>
2018-12-23 10:43:36 +0800

    finish Remove Duplicates from sorted array

---
+++ LeetCodeTests/RemoveDuplicatesFromSortedArray.swift
@@ -0,0 +1,71 @@
+//
+//  RemoveDuplicatesFromSortedArray.swift
+//  LeetCodeTests
+//
+//  Created by Tianchi Wang on 2018/12/21.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+import XCTest
+@testable import LeetCode
+
+class RemoveDuplicatesFromSortedArray: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        var nums = [0,0,1,1,1,2,2,3,3,4]
+        let length = removeDuplicates.removeDuplicates(&nums)
+        var nums2 = [0,0,1,1,1,2,2,3,3,4]
+        let length2 = removeDuplicates.removeDuplicates2(&nums2)
+        XCTAssertEqual(length, 5)
+        XCTAssertEqual(length2, 5)
+    }
+
+    func testCase1() {
+        var nums = [1, 1]
+        let length = removeDuplicates.removeDuplicates(&nums)
+        var nums2 = [1, 1]
+        let length2 = removeDuplicates.removeDuplicates2(&nums2)
+        XCTAssertEqual(length, 1)
+        XCTAssertEqual(length2, 1)
+    }
+    
+    func testCase2() {
+        var nums = [1, 1, 1]
+        let length = removeDuplicates.removeDuplicates(&nums)
+        var nums2 = [1, 1, 1]
+        let length2 = removeDuplicates.removeDuplicates2(&nums2)
+        XCTAssertEqual(length, 1)
+        XCTAssertEqual(length2, 1)
+    }
+    
+    func testCase3() {
+        var nums = [0, 0, 0, 0]
+        let length = removeDuplicates.removeDuplicates(&nums)
+        var nums2 = [0, 0, 0, 0]
+        let length2 = removeDuplicates.removeDuplicates2(&nums2)
+
+        XCTAssertEqual(length, 1)
+        XCTAssertEqual(length2, 1)
+    }
+
+
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
