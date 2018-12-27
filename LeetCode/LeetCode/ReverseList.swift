89d8f6a9c38470f55d14abe7a3fe63724a472c43
RandallWang <randallwang93@gmail.com>
2018-12-27 23:29:52 +0800

    MDZZ_Version Solve

---
+++ LeetCode/LeetCode/ReverseList.swift
@@ -0,0 +1,60 @@
+//
+//  ReverseList.swift
+//  LeetCode
+//
+//  Created by Tianchi Wang on 2018/12/27.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+import Foundation
+
+/**
+Reverse a singly linked list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+Follow up:
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+*/
+
+class reverseList {
+    func MDZZ_reverseList(_ head: ListNode?) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        
+        var tempNode:ListNode? = head
+        
+        
+        var tempList:[Int] = []
+
+        while tempNode != nil{
+            tempList.append(tempNode!.val)
+            tempNode = tempNode?.next
+        }
+        
+        var newNode: ListNode? = nil
+        var index = tempList.count
+        var tailNode: ListNode? = nil
+        
+        while index > 0 {
+            let val = tempList[index - 1]
+            let node = ListNode.init(val)
+            
+            if newNode == nil {
+                newNode = node
+            }else {
+                tailNode!.next = node
+            }
+            tailNode = node
+            
+            index -= 1
+        }
+        
+        return newNode
+    }
+}
---
+++ LeetCode/SupportClasses/ListNode.swift
@@ -0,0 +1,19 @@
+//
+//  ListNode.swift
+//  LeetCode
+//
+//  Created by Tianchi Wang on 2018/12/27.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+     }
+}
