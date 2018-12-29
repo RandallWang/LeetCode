bfeee20d477d1b2d8b160c9aaaecf1b4ad18ba00
RandallWang <randallwang93@gmail.com>
2018-12-29 11:02:14 +0800

    finish Jewels Stones

---
+++ LeetCode/LeetCode/Jewels and Stones.swift
@@ -0,0 +1,39 @@
+//
+//  Jewels and Stones.swift
+//  LeetCode
+//
+//  Created by Tianchi Wang on 2018/12/28.
+//  Copyright © 2018 Tianchi Wang. All rights reserved.
+//
+
+import Foundation
+/**
+ You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+ 
+ The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+ 
+ Example 1:
+ 
+ Input: J = "aA", S = "aAAbbbb"
+ Output: 3
+ Example 2:
+ 
+ Input: J = "z", S = "ZZ"
+ Output: 0
+ Note:
+ 
+ S and J will consist of letters and have length at most 50.
+ The characters in J are distinct.
+*/
+class JewelsStones {
+    func numJewelsInStones(_ J: String, _ S: String) -> Int {
+        var JewelsCount = 0
+        for char in S {
+            if J.contains(char) {
+                JewelsCount += 1
+            }
+        }
+        
+        return JewelsCount
+    }
+}
